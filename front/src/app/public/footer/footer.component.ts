b87686400bf06213d4a0645dac9b2d3d4774d048
Harold21Mtz <hfmartinezc@ufpso.edu.co>
2023-11-15 23:54:40 -0500

    Se ajusta el header y se agrega el footer

--- front/src/app/public/footer/footer.component.ts
+++ front/src/app/public/footer/footer.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatDialog } from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatIconModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
+
+  constructor(
+    public dialog: MatDialog
+  ) {}
 
 }
