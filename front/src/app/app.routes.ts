53a9dd32a79b0c45c2b73294db7b7f85bcf8734e
Harold21Mtz <hfmartinezc@ufpso.edu.co>
2023-11-16 09:20:58 -0500

    Se agrega statistics

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/pages/home/home.component';
+import {StatisticsComponent} from "./home/pages/statistics/statistics.component";
 
 export const routes: Routes = [
     {path: '', pathMatch: 'full', redirectTo:'/home'},
     { path: 'home', component: HomeComponent },
+  { path: 'statistics', component: StatisticsComponent },
 ];
