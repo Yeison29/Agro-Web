aa76e8c4b905084c47abf0cd8318fd142dd5dc7c
Josue0625 <jcampos@ufpso.edu.co>
2023-11-20 13:37:18 -0500

    se hicieron retoques del home

--- front/src/app/home/constants/carousel.const.ts
+++ front/src/app/home/constants/carousel.const.ts
@@ -4,10 +4,10 @@ export const CAROUSEL_DATA_ITEMS : IcarruselItem[] = [
     {
         id: 1,
         title: {
-            first: "Buenas Practicas",
-            second: "Agricolas",
+            first: "Planifica tus",
+            second: "Cosechas",
         },
-        subtitle: "Descripción del carrusel",
+        subtitle: "Facilitamos la planificación estratégica de cosechas, ayudándote a tomar decisiones informadas sobre qué cultivar en diferentes períodos.",
         link: "/",
         image: "assets/images/img1.jpg",
         marginLeft: 0
@@ -15,10 +15,10 @@ export const CAROUSEL_DATA_ITEMS : IcarruselItem[] = [
     {
         id: 1,
         title: {
-            first: "Primero",
-            second: "Segundo",
+            first: "Regulación de",
+            second: "Precios",
         },
-        subtitle: "Descripción del carrusel",
+        subtitle: "Contribuimos a la estabilidad del mercado agrícola al ofrecer herramientas para entender y predecir la producción, impactando positivamente en la fijación de precios.",
         link: "/",
         image: "assets/images/img5.jpg",
         marginLeft: 0
@@ -26,10 +26,10 @@ export const CAROUSEL_DATA_ITEMS : IcarruselItem[] = [
     {
         id: 1,
         title: {
-            first: "Primero",
-            second: "Segundo",
+            first: "Analiza la producción",
+            second: "futura",
         },
-        subtitle: "Descripción del carrusel",
+        subtitle: "Proporcionamos gráficos detallados de producción futura por semana para cada cultivo, permitiéndote tomar decisiones informadas sobre la viabilidad de la siembra.",
         link: "/",
         image: "assets/images/img2.jpg",
         marginLeft: 0
@@ -37,10 +37,10 @@ export const CAROUSEL_DATA_ITEMS : IcarruselItem[] = [
     {
         id: 1,
         title: {
-            first: "Primero",
-            second: "Segundo",
+            first: "Producción",
+            second: "Eficiente",
         },
-        subtitle: "Descripción del carrusel",
+        subtitle: "Contribuimos a la eficiencia en la producción de alimentos al brindar herramientas para gestionar y planificar cosechas, asegurando una oferta alimentaria constante y de calidad.",
         link: "/",
         image: "assets/images/img3.jpg",
         marginLeft: 0
