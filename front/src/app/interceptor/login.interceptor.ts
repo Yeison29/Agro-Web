af1bc37f6243abc0b91a8ed85cb2c1d0ac6615fa
Josue0625 <jcampos@ufpso.edu.co>
2023-11-21 19:05:52 -0500

    se hizo el loader

--- front/src/app/interceptor/login.interceptor.ts
+++ front/src/app/interceptor/login.interceptor.ts
@@ -7,18 +7,19 @@ import {
 } from '@angular/common/http';
 import { Observable, finalize } from 'rxjs';
 import { Token } from '@angular/compiler';
+import { LoaderService } from '../public/loader.service';
 
 @Injectable()
 export class LoginInterceptor implements HttpInterceptor {
 
 tokenLessRoutes: string[] = ['/token' , '/create-user', '/get-all-type-documents', '/get-all-gender', '/get-all-contries', '/get-all-departments', '/get-all-municipalities'];
 
-  // constructor(private load: LoaderService) {}
+   constructor(private load: LoaderService) {}
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
 
 
-    // this.load.setActive();
+    this.load.setActive();
     console.log(request.url.split('?')[0])
 
     if (this.tokenLessRoutes.some(ruta => ruta === request.url.split('?')[0])) {
@@ -37,7 +38,7 @@ tokenLessRoutes: string[] = ['/token' , '/create-user', '/get-all-type-documents
 
     return next.handle(cloneReq).pipe(
       finalize(()=>{
-        // this.load.setInactive(); 
+        this.load.setInactive(); 
       })
     );
   }
